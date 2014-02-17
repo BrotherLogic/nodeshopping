01911fb5d6ef7d6a990ab89e827c8dadd9f23df5
Simon Tucker <brotherlogic@gmail.com>
2014-02-17 06:41:41 -0800

    Working on getting the basic run working

--- app.js
+++ app.js
@@ -17,53 +17,135 @@ var connection_string = '127.0.0.1:27017/myapp';
 var db = mongojs(connection_string,['myapp']);
 var users = db.collection("user");
 var lists = db.collection("list");
-
+var listItems = db.collection("items");
+var diffs = db.collection("diffs");
 
 server.put({path: '/user', version: '0.0.1'}, addUser);
 server.put({path: '/list', version: '0.0.1'}, addList);
-server.get({path: '/list/:id', version: '0.0.1'}, getList);
+server.get({path: '/list/:id/:timestamp', version: '0.0.1'}, getList);
 server.get({path: '/list',version : '0.0.1'},getListForUser);
+server.put({path: '/list/:listid/addItem', version: '0.0.1'}, addItemToList)
 
-function getListForUser(req,res,next) {
-    res.setHeader('Access-Control-Allow-Origin','*');
+function addItemToList(req,res,next) {
+
+	console.log("adding item to list");
+
+	diff = {};
+	diff.description = req.params.description;
+	diff.number = req.params.number;
+
+	listid = req.params.listid;
+
+	console.log("Updating " + diff + " and " + listid);
+
+	givenDiff = updateItem(diff,listid);
+
+	console.log("updated item");
+
+	res.send(200,givenDiff);
+	return next();
 }
 
+function updateItem(diff,listid) {
+
+	//add the diff to the diff table
+	diff = diff;
+	diff['timestamp'] = new Date().getTime();
+	diff['listid'] = listid;
+
+	diffs.save(diff);
+
+	//Adjust the list accordingly
+	list_item = listItems.findOne({description: diff.description, listid:listid});
+
+	console.log("Found " + list_item + " from " + diff.description + " and " + listid);
+	console.log("Also " + (diff.description instanceof String));
+	console.log("Also " + (listid instanceof String));
+
+	if (list_item == null) {
+
+		//Add the item to the list
+		newItem = {};
+		newItem.number = diff.number;
+		newItem.description = diff.description;
+		newItem.listid = listid;
+
+		listItems.save(newItem);
+		return diff;
+	} else {
+		//Update the item
+		list_item.number += diff.number;
+
+		if (list_item.number <= 0) {
+			//Delete the item
+			lists.delete(list_item);
+		} else {
+			lists.update(list_item);
+		}
+
+		return diff;
+	}
+
+
+}
 
 function getList(req,res,next) {
-    res.setHeader('Access-Control-Allow-Origin','*');
+    console.log("getList");
 
-    lists.findOne({_id:mongojs.ObjectId(req.params.id)}, function(err,doc){
+    res.setHeader('Access-Control-Allow-Origin','*')
+
+    diffs.findOne({_id:mongojs.ObjectId(req.params.id),timestamp:{$gt:Number(req.params.timestamp)}}, function(err,doc){
 	    console.log('Response success ' + doc);
 	    console.log('Response error ' + err);
 	    if (doc) {
-		res.send(200,doc);
-		return next();
+			res.send(200,doc);
+			return next();
 	    } else {
-		return next(err);
+			return next(err);
 	    }
 
 	});
-}   
+}  
 
-function addList(req,res,next){
+function getListForUser(req,res,next) {
     res.setHeader('Access-Control-Allow-Origin','*');
     
-    var list = {};
-    list.users = [req.params.userid];
-    list.items = [];
+    console.log("Blah = " + {users:req.params.userid});
 
-    lists.save(list,function(err,success) {
-	    console.log('Response success ' + success);
-	    console.log('Response error ' + err);
-	    if (success) {
-		res.send(201,list);
-		return next();
-	    } else {
-		return next(err);
-	    }
-	});
+    lists.find({users:Number(req.params.userid)}, function(err,doc){
+
+    	console.log('Lists response success for ' + req.params.userid + ': ' + doc);
+    	console.log('List response error ' + err);
+
+    	if (doc) {
+    		res.send(200,doc);
+    		return next();
+    	} else {
+    		return next(err);
+    	}
+    });
+
+    return next();
 }
-   
+
+function addList(userids)
+{
+	//Create a default list for this user
+	var list = {};
+	list.users = [req.params.id];
+	list.items = [];
+
+    lists.save(list, function(err,success){
+		console.log('Response success ' + success);
+		console.log('Response error ' + err);
+		if (success) {
+				res.send(201,user);
+				return next();
+			} else {
+				return next(err);
+			}
+		});
+}
 
 function addUser(req,res,next){
     res.setHeader('Access-Control-Allow-Origin','*');
@@ -72,19 +154,34 @@ function addUser(req,res,next){
     user.name = req.params.name;
     user.url = req.params.url;
 
-    users.save(user, function(err,success) {
-	    console.log('Response success ' + success);
-	    console.log('Response error ' + err);
-	    if (success) {
-		res.send(201,user);
-		return next();
-	    } else {
-		return next(err);
-	    }
-	});
+    // Check to see if the user already exists
+    users.findOne({id:user.id}, function(err,doc) {
+    	console.log('Response error ' + err);
+    	console.log('Doc ret ' + doc);
+
+    	//If the user is new - ensure that at least one list is created
+    	if (doc != null) {
+    		res.send(200,doc);
+    		return next();
+    	} else {
+    		//Storing user
+    		console.log('Storing user ' + user);
+    		users.save(user, function(err,success) {
+	   			console.log('Response success ' + success);
+	    		console.log('Response error ' + err);
+	    		if (success) {
+	    			addList([req.params.id]);
+	    		} else {
+					return next(err);
+	    		}
+			});
+    	}
+    });
+
+    
 }
 
 
 server.listen(port ,ip_addr, function(){
 	console.log('%s listening at %s ', server.name , server.url);
-    });
+});
--- etc/testscript.py
+++ etc/testscript.py
@@ -12,3 +12,12 @@ print r.content
 #2. Get the lists that the user belongs to
 r = requests.get(SERVER_LOC + "list?userid=1234567",headers=headers)
 print r.content
+list_id = json.loads(r.content)[0]['_id']
+
+#3. Add two items to the list
+payload = '{"description":"apples","number":3}';
+r = requests.put(SERVER_LOC +"list/" + list_id + "/addItem",data=payload,headers=headers);
+print r.content
+payload = '{"description":"bananas","number":3}';
+r = requests.put(SERVER_LOC +"list/" + list_id + "/addItem",data=payload,headers=headers);
+print r.content
